9b72bf1a7d2a42dc5308a0f2d173f603a63cc351
Sai Kumar Manakan <msk.mymails@gmail.com>
2015-10-21 17:52:34 -0500

    Added startup scripts

---
+++ HTTPProject/src/HTTPWebServer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ ! -f HTTPWebServer.class ]; then
+	echo Compiling HTTPWebServer...
+    javac HTTPWebServer.java HTTPRequest.java
+fi
+echo Starting HTTPWebServer...
+java -cp . HTTPWebServer
+
